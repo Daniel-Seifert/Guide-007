e13a066a535efef2b0221eb6b32aa18814d346a1
Cavvar <denis.angeletta@googlemail.com>
2020-03-05 21:30:48 +0100

    reverted to a3bea153cda68ed2b6f652b4e8493f84bd083291

--- src/app/login/Login/Login.vue
+++ src/app/login/Login/Login.vue
@@ -1,55 +1,37 @@
 <template>
-  <div>
-    <v-card max-width="400">
-      <v-card-title class="justify-center">
-        <h3>ZPA-Login</h3>ADD HM LOGO HERE
-      </v-card-title>
-      <v-card-text>
-        <v-text-field label="Username" prepend-icon="account_circle" v-model="username" />
-        <v-text-field
-          label="Password"
-          type="Password"
-          prepend-icon="lock"
-          append-icon="visibility_off"
-          v-model="password"
-        />
-      </v-card-text>
-      <v-card-actions>
-        <v-btn class="white--text" color="red darken-1" v-on:click="doLogin()">Login</v-btn>
-      </v-card-actions>
-    </v-card>
-  </div>
+    <div>
+        <v-card max-width="400">
+            <v-card-title class="justify-center">
+                <h3>ZPA-Login</h3>ADD HM LOGO HERE
+            </v-card-title>
+            <v-card-text>
+                <v-text-field label="Username" prepend-icon="account_circle" v-model="username" />
+                <v-text-field
+                        label="Password"
+                        type="Password"
+                        prepend-icon="lock"
+                        append-icon="visibility_off"
+                        v-model="password"
+                />
+            </v-card-text>
+            <v-card-actions>
+                <v-btn class="white--text" color="red darken-1" v-on:click="doLogin()">Login</v-btn>
+            </v-card-actions>
+        </v-card>
+    </div>
 </template>
 
 <script lang="ts">
-import { mapState, mapActions } from 'vuex';
-import { IState } from '@/app/state';
-
-export default {
-  metaInfo: {
-    title: 'Guide-007',
-  },
-  name: 'Login',
-  data(): any {
-    return {
-      username: '',
-      password: '',
-    };
-  },
-  methods: {
-    ...mapActions('auth', ['login']),
-    async doLogin() {
-      const formData = {
-        username: this.username,
-        password: this.password,
-      };
-      try {
-        await this.login(formData);
-        this.$router.push('/');
-      } catch (err) {
-        console.log(err);
-      }
+  export default {
+    metaInfo: {
+      title: 'Guide-007',
     },
-  },
-};
+    name: 'Login',
+    data(): any {
+      return {
+        username: '',
+        password: '',
+      };
+    },
+  };
 </script>
